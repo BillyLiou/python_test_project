db4962e633a32aa3e2f594e806887ccd801033f1
BillyLiou <billy91281003@gmail.com>
2020-10-02 12:54:58 +0800

    first commit in this python project

---
+++ test_project/REGPRACTICE.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  2 12:20:55 2020
+
+@author: billyliou
+"""
+
+import re
+
+
+pattern = r"abc"
+res = re.match(pattern, 'abcabc',flags=0);
+if res:
+    print(res.group())
+    print(res.group(1))
+
---
+++ test_project/test.py
@@ -0,0 +1,22 @@
+# class Polygon(object):
+#     def __init__(self,id):
+#         self.id = id
+#     def show(self):
+#         print("Id is: "+str(self.id))
+
+
+# class Rectangle(Polygon):
+#     def __init__(self,id):
+#         super(Rectangle,self).__init__(id)
+
+#     def show(self):
+#         super().show()
+#         print("subclass show")
+
+
+# r =  Rectangle(5)
+# r.show()
+
+
+
+
