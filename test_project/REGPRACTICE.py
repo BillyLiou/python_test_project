c8b6478d773deee533363f2e0bbe7181bc2a6887
BillyLiou <billy91281003@gmail.com>
2020-10-02 14:21:05 +0800

    update .spyderconfig

--- test_project/REGPRACTICE.py
+++ test_project/REGPRACTICE.py
@@ -9,8 +9,8 @@ Created on Fri Oct  2 12:20:55 2020
 import re
 
 
-pattern = r"abc"
-res = re.match(pattern, 'abcabc',flags=0);
+pattern = r'abc'
+res = re.match(pattern, 'abcabc',flags=0)
 if res:
     print(res.group())
     print(res.group())
