d4345418e0f2030322a92d6a65ff0b26c7604b85
劉品軒 <billyliou@BillyLiu.local>
2020-11-09 11:20:49 +0800

    增加python header註解

---
+++ test_project/LambdaPractice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   LambdaPractice.py
+@Time    :   2020/11/09 11:20:04
+@Author  :   Billy Liu
+@Version :   1.0
+@Contact :   billy@test.mail
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   None
+'''
