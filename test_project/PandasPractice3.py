a2d2c50450d0e1259bf5a3e1a927555b1c470d5c
劉品軒 <billyliou@BillyLiu.local>
2021-02-04 00:15:29 +0800

    新增sort排序的方法練習 in PandasPractice3.py

--- test_project/PandasPractice3.py
+++ test_project/PandasPractice3.py
@@ -44,31 +44,42 @@ print(sales)
 # print(sales.describe())
 # print()
 
-# pandas iloc()
-print('========== iloc ==========')
+# # pandas iloc()
+# print('========== iloc ==========')
 
-# 應該怎麼記呢,index第幾個的第幾個欄位,
-# 第一個參數是第幾個index也就是0個 = Billy
-# 第二個參數是第幾個欄位也就是1
-print(sales.iloc[0,1])
+# # 應該怎麼記呢,index第幾個的第幾個欄位,
+# # 第一個參數是第幾個index也就是0個 = Billy
+# # 第二個參數是第幾個欄位也就是1
+# print(sales.iloc[0,1])
 
-# 若想知道前兩個的第一欄位可以有[0:1,1]去找
-print(sales.iloc[0:2,[1]])
-
-
-print('========== loc ==========')
-# loc也是一樣的,差別在iloc是以數字(index)作為篩選標準,loc是以行列的title
-# 第一個參數是第幾個index也就是0個 = Billy
-# 第二個參數是第幾個欄位也就是1
-
-# 先求算出有哪幾個red_wine是大於6的
-print(sales.loc[:,"red_wine"]>6)
-
-# 列出滿足red_wine大於6的列
-print(sales[sales.loc[:,"red_wine"]>6])
-print()
-
-# 列出滿足red_wine大於6的那些列但只列出white_wine的數字
-print(sales[sales.loc[:,"red_wine"]>6].loc[:,['white_wine']])
+# # 若想知道前兩個的第一欄位可以有[0:1,1]去找
+# print(sales.iloc[0:2,[1]])
 
 
+# print('========== loc ==========')
+# # loc也是一樣的,差別在iloc是以數字(index)作為篩選標準,loc是以行列的title
+# # 第一個參數是第幾個index也就是0個 = Billy
+# # 第二個參數是第幾個欄位也就是1
+
+# # 先求算出有哪幾個red_wine是大於6的
+# print(sales.loc[:,"red_wine"]>6)
+
+# # 列出滿足red_wine大於6的列
+# print(sales[sales.loc[:,"red_wine"]>6])
+# print()
+
+# # 列出滿足red_wine大於6的那些列但只列出white_wine的數字
+# print(sales[sales.loc[:,"red_wine"]>6].loc[:,['white_wine']])
+
+
+print('========== sort_index ==========')
+
+# 可以將已經列出的pd照著某些規則排列,ascending=True or False
+print(sales.sort_values(by = 'cup_cost',ascending=False))
+
+# sort_index可以排列左側(index的這欄)
+# axis 0 會依index排列也就是Billy Charly John Tim
+print(sales.sort_index(axis=0,ascending=True))
+
+# axis 1 會依欄位排列也就是上面會變成cup_cost red_wine white_wine的順序
+print(sales.sort_index(axis=1,ascending=True))
